d2d2e9225bdd7e7b85c300a44716f21a227a94e6
ashenmalaka <37531139+ashenmalaka@users.noreply.github.com>
2019-09-07 18:59:02 +0530

    Coded for slider scrolling

--- js/main.js
+++ js/main.js
@@ -8,3 +8,13 @@ $(".slider-one")
     prevArrow: ".site-slider .slider-btn .prev",
     nextArrow: ".site-slider .slider-btn .next"
 });
+
+//Fisrt Slider
+$(".slider-two")
+.not(".slick-intialized")
+.slick({
+    prevArrow: ".site-slider.two .prev",
+    nextArrow: ".site-slider.two .next",
+    slidesToshow: 5,
+    slidesToScroll: 1
+});
